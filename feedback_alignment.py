030bf890c109541e9f5bc5dd7d403890c9078095
程泽华 <chengzehua@outlook.com>
2020-02-16 00:51:29 +0000

    :tada: Add Experiment for FA.

--- feedback_alignment.py
+++ feedback_alignment.py
@@ -1,74 +1,40 @@
 import torch
-from torch import nn
+import torch.nn as nn
 from torch.autograd import Function
-
-
-class LinearFunction(Function):
-    @staticmethod
-    # bias is an optional argument
-    def forward(ctx, input, weight, bias=None):
-        ctx.save_for_backward(input, weight, bias)
-        output = input.mm(weight.t())
-        if bias is not None:
-            output += bias.unsqueeze(0).expand_as(output)
-        return output
-
-    # This function has only a single output, so it gets only one gradient
-    @staticmethod
-    def backward(ctx, grad_output):
-        # This is a pattern that is very convenient - at the top of backward
-        # unpack saved_tensors and initialize all gradients w.r.t. inputs to
-        # None. Thanks to the fact that additional trailing Nones are
-        # ignored, the return statement is simple even when the function has
-        # optional inputs.
-        input, weight, bias = ctx.saved_variables
-        grad_input = grad_weight = grad_bias = None
-        # These needs_input_grad checks are optional and there only to
-        # improve efficiency. If you want to make your code simpler, you can
-        # skip them. Returning gradients for inputs that don't require it is
-        # not an error.
-        if ctx.needs_input_grad[0]:
-            grad_input = grad_output.mm(weight)
-        if ctx.needs_input_grad[1]:
-            grad_weight = grad_output.t().mm(input)
-        if bias is not None and ctx.needs_input_grad[2]:
-            grad_bias = grad_output.sum(0)
-
-        return grad_input, grad_weight, grad_bias
 
 
 class LinearFAFunction(Function):
     @staticmethod
-    # Same as reference linear function, but with additional feed forward tensor for backward
-    def forward(ctx, input, weight, weight_fa, bias=None):
-        ctx.save_for_backward(input, weight, weight_fa, bias)
-        output = input.mm(weight.t())
+    # same as reference linear function, but with additional fa tensor for backward
+    def forward(context, input, weight, weight_fa, bias=None):
+        context.save_for_backward(input, weight, weight_fa, bias)
+        output = input.matmul(weight.t())
         if bias is not None:
             output += bias.unsqueeze(0).expand_as(output)
         return output
 
     @staticmethod
-    def backward(ctx, grad_output):
-        input, weight, weight_fa, bias = ctx.saved_variables
+    def backward(context, grad_output):
+        input, weight, weight_fa, bias = context.saved_variables
         grad_input = grad_weight = grad_weight_fa = grad_bias = None
-
-        if ctx.needs_input_grad[0]:
-            # Calculate the gradient of input with fixed feedback alignment tensor,
-            # rather than the "correct" model weight
-            grad_input = grad_output.mm(weight_fa)
-        if ctx.needs_input_grad[1]:
+        if context.needs_input_grad[0]:
+            # all of the logic of FA resides in this one line
+            # calculate the gradient of input with fixed fa tensor, rather than the "correct" model weight
+            grad_input = grad_output.matmul(weight_fa)
+        if context.needs_input_grad[1]:
             # grad for weight with FA'ed grad_output from downstream layer
             # it is same with original linear function
-            grad_weight = grad_output.t().mm(input)
-        if bias is not None and ctx.needs_input_grad[2]:
+            grad_weight = grad_output.t().matmul(input)
+        if bias is not None and context.needs_input_grad[3]:
             grad_bias = grad_output.sum(0).squeeze(0)
 
         return grad_input, grad_weight, grad_weight_fa, grad_bias
 
 
-class LinearFeedbackAlignmentModule(nn.Module):
+class FeedbackAlignmentLinear(nn.Module):
+
     def __init__(self, input_features, output_features, bias=True):
-        super(LinearFeedbackAlignmentModule, self).__init__()
+        super(FeedbackAlignmentLinear, self).__init__()
         self.input_features = input_features
         self.output_features = output_features
 
@@ -82,52 +48,12 @@ class LinearFeedbackAlignmentModule(nn.Module):
 
         # fixed random weight and bias for FA backward pass
         # does not need gradient
-        self.weight_fa = torch.FloatTensor(output_features, input_features)
-
+        self.weight_fa = torch.rand(output_features, input_features, requires_grad=False).cuda()
+        # self.weight_fa = Variable(torch.Tensor(output_features, input_features), requires_grad=False)
         # weight initialization
-        torch.nn.init.kaiming_uniform(self.weight)
-        torch.nn.init.kaiming_uniform(self.weight_fa)
-        torch.nn.init.constant(self.bias, 1)
+        torch.nn.init.kaiming_uniform_(self.weight)
+        torch.nn.init.kaiming_uniform_(self.weight_fa)
+        torch.nn.init.constant_(self.bias, 1)
 
     def forward(self, input):
         return LinearFAFunction.apply(input, self.weight, self.weight_fa, self.bias)
-
-
-class LinearFeedbackAlignment(nn.Module):
-    """
-    Linear feed-forward networks with feedback alignment learning
-    Does NOT perform non-linear activation after each layer
-    """
-
-    def __init__(self, in_features, num_layers, num_hidden_list):
-        """
-        :param in_features: dimension of input features (784 for MNIST)
-        :param num_layers: number of layers for feed-forward net
-        :param num_hidden_list: list of integers indicating hidden nodes of each layer
-        """
-        super(LinearFeedbackAlignment, self).__init__()
-        self.in_features = in_features
-        self.num_layers = num_layers
-        self.num_hidden_list = num_hidden_list
-
-        # create list of linear layers
-        # first hidden layer
-        self.linear = [LinearFeedbackAlignmentModule(self.in_features, self.num_hidden_list[0])]
-        # append additional hidden layers to list
-        for idx in range(self.num_layers - 1):
-            self.linear.append(LinearFeedbackAlignmentModule(self.num_hidden_list[idx], self.num_hidden_list[idx + 1]))
-
-        # create ModuleList to make list of layers work
-        self.linear = nn.ModuleList(self.linear)
-
-    def forward(self, inputs):
-        """
-        forward pass, which is same for conventional feed-forward net
-        :param inputs: inputs with shape [batch_size, in_features]
-        :return: logit outputs from the network
-        """
-        # first layer
-        linear1 = self.linear[0](inputs)
-        # second layer
-        linear2 = self.linear[1](linear1)
-        return linear2
---
+++ main.py
@@ -0,0 +1,178 @@
+import argparse
+import os
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn.functional as F
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+from utils.bar_show import progress_bar
+
+from feedback_alignment import FeedbackAlignmentLinear
+
+parser = argparse.ArgumentParser(description='DL without Weight Transport PyTorch Implementation.')
+
+parser.add_argument('--root_dir', type=str, default='./')
+parser.add_argument('--data_dir', type=str, default='./data')
+parser.add_argument('--log_name', type=str, default='', required=True)
+parser.add_argument('--pretrain', action='store_true', default=False)
+parser.add_argument('--pretrain_dir', type=str, default='CIFAR10_pretrain')
+parser.add_argument('--method', type=str, default='fa', required=True)
+parser.add_argument('--lr', type=float, default=0.1)
+parser.add_argument('--wd', type=float, default=5e-4)
+parser.add_argument('-b', type=int, default=256, help="Batch Size")
+parser.add_argument('--eval_batch_size', type=int, default=100)
+parser.add_argument('--max_epochs', type=int, default=180)
+parser.add_argument('--log_interval', type=int, default=40)
+parser.add_argument('--num_workers', type=int, default=12)
+
+cfg = parser.parse_args()
+
+best_acc = 0  # best test accuracy
+start_epoch = 0
+
+cfg.log_dir = os.path.join(cfg.root_dir, 'logs', cfg.log_name)
+cfg.ckpt_dir = os.path.join(cfg.root_dir, 'ckpt', cfg.pretrain_dir)
+
+os.makedirs(cfg.log_dir, exist_ok=True)
+os.makedirs(cfg.ckpt_dir, exist_ok=True)
+
+
+def get_dataloader(root, batch_szie, num_workers):
+    train_transform_list = [transforms.RandomHorizontalFlip(),
+                            transforms.RandomCrop(32, padding=4),
+                            transforms.RandomRotation(15),
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)), ]
+    eval_transform_list = [transforms.ToTensor(),
+                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)), ]
+    train_transform_list = transforms.Compose(train_transform_list)
+    eval_transform_list = transforms.Compose(eval_transform_list)
+    train_dataset = CIFAR10(root=root, train=True, download=True,
+                            transform=train_transform_list)
+    train_loader = DataLoader(train_dataset, batch_size=batch_szie, shuffle=True,
+                              num_workers=num_workers, pin_memory=True)
+
+    eval_dataset = CIFAR10(root=root, train=False, download=True,
+                           transform=eval_transform_list)
+    eval_loader = DataLoader(eval_dataset, batch_size=batch_szie, shuffle=False,
+                             num_workers=num_workers, pin_memory=True)
+    return train_loader, eval_loader
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.fc1 = FeedbackAlignmentLinear(3 * 32 * 32, 120)
+        self.fc2 = FeedbackAlignmentLinear(120, 10)
+
+    def forward(self, x):
+        print(x.shape)
+        x = x.view(-1, 3 * 32 * 32)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+def main():
+    train_loader, eval_loader = get_dataloader(cfg.data_dir, cfg.b, cfg.num_workers)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    model = Model().to(device)
+
+    if device == 'cuda':
+        model = torch.nn.DataParallel(model)
+        cudnn.benchmark = True
+
+    optimizer = torch.optim.SGD(model.parameters(),
+                                lr=1e-4, momentum=0.9, weight_decay=0.001, nesterov=True)
+    criterion = torch.nn.CrossEntropyLoss()
+    lr_schedu = torch.optim.lr_scheduler.MultiStepLR(optimizer, [90, 150, 200], gamma=0.1)
+    summary_writer = SummaryWriter(cfg.log_dir)
+
+    if cfg.pretrain:
+        ckpt = torch.load(os.path.join(cfg.ckpt_dir, f'checkpoint.t7'))
+        model.load_state_dict(ckpt['model_state_dict'])
+        optimizer.load_state_dict(ckpt['optimizer_state_dict'])
+        start_epoch = ckpt['epoch']
+        print('===> Load last checkpoint data')
+    else:
+        start_epoch = 0
+        print('===> Start from scratch')
+
+    def train(epoch):
+        print('\nEpoch: %d' % epoch)
+        model.train()
+        train_loss, correct, total = 0, 0, 0
+        for batch_idx, (inputs, targets) in enumerate(train_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()  # compute the .grad for all weights
+            optimizer.step()
+
+            train_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(train_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+            if batch_idx % cfg.log_interval == 0:  # every log_interval mini_batches...
+                summary_writer.add_scalar('Loss/train', train_loss / (batch_idx + 1),
+                                          epoch * len(train_loader) + batch_idx)
+                summary_writer.add_scalar('Accuracy/train', 100. * correct / total,
+                                          epoch * len(train_loader) + batch_idx)
+                summary_writer.add_scalar('learning rate', optimizer.param_groups[0]['lr'],
+                                          epoch * len(train_loader) + batch_idx)
+
+    def test(epoch):
+        global best_acc
+        model.eval()
+
+        test_loss, correct, total = 0, 0, 0
+        with torch.no_grad():
+            for batch_idx, (inputs, targets) in enumerate(eval_loader):
+                inputs, targets = inputs.to(device), targets.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, targets)
+
+                test_loss += loss.item()
+                _, predicted = outputs.max(1)
+                total += targets.size(0)
+                correct += predicted.eq(targets).sum().item()
+
+                progress_bar(batch_idx, len(eval_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                             % (test_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+                if batch_idx % cfg.log_interval == 0:  # every log_interval mini_batches...
+                    summary_writer.add_scalar('Loss/test', test_loss / (batch_idx + 1),
+                                              epoch * len(train_loader) + batch_idx)
+                    summary_writer.add_scalar('Accuracy/test', 100. * correct / total,
+                                              epoch * len(train_loader) + batch_idx)
+        acc = 100. * correct / total
+        if acc > best_acc:
+            print('Saving Models..')
+            state = {
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'acc': acc,
+                'epoch': epoch,
+            }
+            torch.save(state, os.path.join(cfg.ckpt_dir, '{}_checkpoint.t7'.format(cfg.log_name)))
+            best_acc = acc
+
+    for epoch in range(start_epoch, cfg.max_epochs):
+        train(epoch)
+        test(epoch)
+        lr_schedu.step(epoch)
+    summary_writer.close()
+
+
+if __name__ == '__main__':
+    main()
